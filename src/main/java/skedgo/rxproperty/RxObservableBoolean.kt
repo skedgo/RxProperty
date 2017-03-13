58e3dee0240b178b823cfe9cb827fd5a40fd22b7
Thuy Trinh <thuy@skedgo.com>
2017-03-13 19:29:09 +0700

    Write some initial docs

--- src/main/java/skedgo/rxproperty/RxObservableBoolean.kt
+++ src/main/java/skedgo/rxproperty/RxObservableBoolean.kt
@@ -4,8 +4,14 @@ import android.databinding.ObservableBoolean
 import rx.Observable
 import rx.subscriptions.Subscriptions
 
+/**
+ * When we subscribe to an [Observable] created by this function,
+ * the [Observable] will emit the current value of [ObservableBoolean]
+ * and also emit any future changes.
+ */
 fun ObservableBoolean.asObservable(): Observable<Boolean> {
   return Observable.create {
+    // To emit the current value.
     it.onNext(get())
     val callback = object : android.databinding.Observable.OnPropertyChangedCallback() {
       override fun onPropertyChanged(sender: android.databinding.Observable?, propertyId: Int) {
--- src/main/java/skedgo/rxproperty/RxObservableField.kt
+++ src/main/java/skedgo/rxproperty/RxObservableField.kt
@@ -4,8 +4,14 @@ import android.databinding.ObservableField
 import rx.Observable
 import rx.subscriptions.Subscriptions
 
+/**
+ * When we subscribe to an [Observable] created by this function,
+ * the [Observable] will emit the current value of [ObservableField]
+ * and also emit any future changes.
+ */
 fun <T> ObservableField<T>.asObservable(): Observable<T> {
   return Observable.create {
+    // To emit the current value.
     it.onNext(get())
     val callback = object : android.databinding.Observable.OnPropertyChangedCallback() {
       override fun onPropertyChanged(sender: android.databinding.Observable?, propertyId: Int) {
