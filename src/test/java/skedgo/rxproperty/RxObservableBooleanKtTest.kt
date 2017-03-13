ad4cadfa6ba3af39e0d2617a7b0ea00e03199199
Thuy Trinh <thuy@skedgo.com>
2017-03-13 19:22:59 +0700

    Write initial tests for `ObservableBoolean.asObservable()`

---
+++ src/test/java/skedgo/rxproperty/RxObservableBooleanKtTest.kt
@@ -0,0 +1,40 @@
+package skedgo.rxproperty
+
+import android.databinding.ObservableBoolean
+import org.junit.Test
+import rx.observers.TestSubscriber
+
+class RxObservableBooleanKtTest {
+  @Test fun shouldEmitCurrentValueAsFalse() {
+    val toBeOrNotToBe = ObservableBoolean()
+
+    val subscriber = TestSubscriber<Boolean>()
+    toBeOrNotToBe.asObservable().subscribe(subscriber)
+
+    subscriber.assertValue(false)
+  }
+
+  @Test fun shouldEmitCurrentValueAsTrue() {
+    val toBeOrNotToBe = ObservableBoolean(true)
+
+    val subscriber = TestSubscriber<Boolean>()
+    toBeOrNotToBe.asObservable().subscribe(subscriber)
+
+    subscriber.assertValue(true)
+  }
+
+  @Test fun shouldEmitFutureChanges() {
+    val toBeOrNotToBe = ObservableBoolean()
+
+    val subscriber = TestSubscriber<Boolean>()
+    toBeOrNotToBe.asObservable().subscribe(subscriber)
+
+    subscriber.assertValue(false)
+
+    toBeOrNotToBe.set(true)
+    subscriber.assertValues(false, true)
+
+    toBeOrNotToBe.set(true)
+    subscriber.assertValues(false, true)
+  }
+}
